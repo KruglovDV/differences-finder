eee9e93c80eda903e7cea775e5523d4b723c6f1b
Dmitry Kruglov <kruglov081289@mail.ru>
2017-06-17 00:15:51 +0300

    changed json render

--- __tests__/json-test.js
+++ __tests__/json-test.js
@@ -1,79 +1,94 @@
-// import path from 'path';
-// import diff from '../src/';
-//
-// const res = {
-//   host: 'hexlet.io',
-//   '+timeout': 20,
-//   '-timeout': 50,
-//   '-proxy': '123.234.53.22',
-//   '+verbose': true,
-// };
-//
-// test('difference between JSON files', () => {
-//   const firstFile = path.join(__dirname, '__fixtures__/first.json');
-//   const secondFile = path.join(__dirname, '__fixtures__/second.json');
-//   expect(diff(firstFile, secondFile, 'json')).toEqual(JSON.stringify(res));
-// });
-//
-// test('difference between YAML files', () => {
-//   const firstFile = path.join(__dirname, '__fixtures__/first.yml');
-//   const secondFile = path.join(__dirname, '__fixtures__/second.yml');
-//   expect(diff(firstFile, secondFile, 'json')).toEqual(JSON.stringify(res));
-// });
-//
-// test('difference between INI files', () => {
-//   const resIni = {
-//     host: 'hexlet.io',
-//     '+timeout': '20',
-//     '-timeout': '50',
-//     '-proxy': '123.234.53.22',
-//     '+verbose': true,
-//   };
-//   const firstFile = path.join(__dirname, '__fixtures__/first.ini');
-//   const secondFile = path.join(__dirname, '__fixtures__/second.ini');
-//   expect(diff(firstFile, secondFile, 'json')).toEqual(JSON.stringify(resIni));
-// });
-//
-// const recRes = {
-//   common: {
-//     setting1: 'Value 1',
-//     '-setting2': 200,
-//     setting3: true,
-//     '-setting6': {
-//       '-key': 'value',
-//     },
-//     '+setting4': 'blah blah',
-//     '+setting5': {
-//       '+key5': 'value5',
-//     },
-//   },
-//   group1: {
-//     '+baz': 'bars',
-//     '-baz': 'bas',
-//     foo: 'bar',
-//   },
-//   '-group2': {
-//     '-abc': 12345,
-//   },
-//   '+group3': {
-//     '+fee': 100500,
-//   },
-// };
-//
-// test('difference between JSON recursive files', () => {
-//   const firstFile = path.join(__dirname, '__fixtures__/recursFirst.json');
-//   const secondFile = path.join(__dirname, '__fixtures__/recursSecond.json');
-//   expect(diff(firstFile, secondFile, 'json')).toEqual(JSON.stringify(recRes));
-// });
-//
-// test('difference between YAML recursive files', () => {
-//   const firstFile = path.join(__dirname, '__fixtures__/recursFirst.yml');
-//   const secondFile = path.join(__dirname, '__fixtures__/recursSecond.yml');
-//   expect(diff(firstFile, secondFile, 'json')).toEqual(JSON.stringify(recRes));
-// });
-//
-// test('difference between INI recursive files', () => {
-//   const firstFile = path.join(__dirname, '__fixtures__/recursFirst.json');
-//   const secondFile = path.join(__dirname, '__fixtures__/recursSecond.json');
-//   expect(diff(firstFile, secondFile, 'json')).toEqual(JSON.stringify(recRes));
-// });
+import path from 'path';
+import diff from '../src/';
+
+const res = {
+  host: { type: 'unchanged', value: 'hexlet.io' },
+  timeout: { type: 'updated', after: 20, before: 50 },
+  proxy: { type: 'removed', value: '123.234.53.22' },
+  verbose: { type: 'added', value: true },
+};
+
+test('difference between JSON files', () => {
+  const firstFile = path.join(__dirname, '__fixtures__/first.json');
+  const secondFile = path.join(__dirname, '__fixtures__/second.json');
+  expect(diff(firstFile, secondFile, 'json')).toEqual(res);
+});
+
+test('difference between YAML files', () => {
+  const firstFile = path.join(__dirname, '__fixtures__/first.yml');
+  const secondFile = path.join(__dirname, '__fixtures__/second.yml');
+  expect(diff(firstFile, secondFile, 'json')).toEqual(res);
+});
+
+test('difference between INI files', () => {
+  const resIni = {
+    host: { type: 'unchanged', value: 'hexlet.io' },
+    timeout: { type: 'updated', after: '20', before: '50' },
+    proxy: { type: 'removed', value: '123.234.53.22' },
+    verbose: { type: 'added', value: true },
+  };
+  const firstFile = path.join(__dirname, '__fixtures__/first.ini');
+  const secondFile = path.join(__dirname, '__fixtures__/second.ini');
+  expect(diff(firstFile, secondFile, 'json')).toEqual(resIni);
+});
+
+const recRes = {
+  common: {
+    type: 'unchanged',
+    value: {
+      setting1: { type: 'unchanged', value: 'Value 1' },
+      setting2: { type: 'removed', value: 200 },
+      setting3: { type: 'unchanged', value: true },
+      setting6: {
+        type: 'removed',
+        value: {
+          key: { type: 'removed', value: 'value' },
+        },
+      },
+      setting4: { type: 'added', value: 'blah blah' },
+      setting5: {
+        type: 'added',
+        value: {
+          key5: { type: 'added', value: 'value5' },
+        },
+      },
+    },
+  },
+  group1: {
+    type: 'unchanged',
+    value: {
+      baz: { type: 'updated', before: 'bas', after: 'bars' },
+      foo: { type: 'unchanged', value: 'bar' },
+    },
+  },
+  group2: {
+    type: 'removed',
+    value: {
+      abc: { type: 'removed', value: 12345 },
+    },
+  },
+  group3: {
+    type: 'added',
+    value: {
+      fee: { type: 'added', value: 100500 },
+    },
+  },
+};
+
+test('difference between JSON recursive files', () => {
+  const firstFile = path.join(__dirname, '__fixtures__/recursFirst.json');
+  const secondFile = path.join(__dirname, '__fixtures__/recursSecond.json');
+  expect(diff(firstFile, secondFile, 'json')).toEqual(recRes);
+});
+
+test('difference between YAML recursive files', () => {
+  const firstFile = path.join(__dirname, '__fixtures__/recursFirst.yml');
+  const secondFile = path.join(__dirname, '__fixtures__/recursSecond.yml');
+  expect(diff(firstFile, secondFile, 'json')).toEqual(recRes);
+});
+
+test('difference between INI recursive files', () => {
+  const firstFile = path.join(__dirname, '__fixtures__/recursFirst.json');
+  const secondFile = path.join(__dirname, '__fixtures__/recursSecond.json');
+  expect(diff(firstFile, secondFile, 'json')).toEqual(recRes);
+});
--- src/render/renderJson.js
+++ src/render/renderJson.js
@@ -1,31 +1,19 @@
+import _ from 'lodash';
+
 const render = (ast) => {
-  const res = Object.keys(ast).reduce((acc, key) => {
-    if (ast[key].type === 'unchanged') {
-      if (typeof ast[key].value === 'object') {
-        return { ...acc, [key]: render(ast[key].value) };
-      }
-      return { ...acc, [key]: ast[key].value };
+  const res = ast.reduce((acc, el) => {
+    const [key, type, value] = _.flattenDepth(el, 1);
+
+    if (typeof value[0] === 'object') {
+      return { ...acc, [key]: { type, value: render(value[0]) } };
     }
 
-    if (ast[key].type === 'changed') {
-      return { ...acc, [`+${key}`]: ast[key].after, [`-${key}`]: ast[key].before };
+    if (type === 'updated') {
+      return { ...acc, [key]: { type, before: value[0], after: value[1] } };
     }
-    if (ast[key].type === 'removed') {
-      if (typeof ast[key].value === 'object') {
-        return { ...acc, [`-${key}`]: render(ast[key].value) };
-      }
-      return { ...acc, [`-${key}`]: ast[key].value };
-    }
-    if (ast[key].type === 'added') {
-      if (typeof ast[key].value === 'object') {
-        return { ...acc, [`+${key}`]: render(ast[key].value) };
-      }
-      return { ...acc, [`+${key}`]: ast[key].value };
-    }
-    return acc;
+    return { ...acc, [key]: { type, value: value[0] } };
   }, {});
-
   return res;
 };
 
-export default ast => JSON.stringify(render(ast));
+export default ast => render(ast);
